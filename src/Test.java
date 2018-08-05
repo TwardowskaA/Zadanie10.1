4ab3801fccbc37985652aaddfb999182b6abd0f3
Agata <twardowska123@gmail.com>
2018-08-05 11:40:33 +0200

    Wyjątki kontrolowane

--- src/Test.java
+++ src/Test.java
@@ -9,11 +9,19 @@ public class Test {
         System.out.println("Podaj nazwisko:");
         String lastName = scan.nextLine();
         if(firstName == null || lastName == null || firstName.length() < 2 || lastName.length() < 2)
-            throw new NameUndefinedException("Imie i nazwisko nie może być nullem i mieć mniej niż 2 znaki");
+            try {
+                throw new NameUndefinedException("Imie i nazwisko nie może być nullem i mieć mniej niż 2 znaki");
+            } catch (NameUndefinedException e) {
+                e.printStackTrace();
+            }
         System.out.println("Podaj wiek:");
         int age = scan.nextInt();
         if( age < 1)
-            throw new IncorrectAgeException("Wiek nie moze byc mniejszy niz 1");
+            try {
+                throw new IncorrectAgeException("Wiek nie moze byc mniejszy niz 1");
+            } catch (IncorrectAgeException e) {
+                e.printStackTrace();
+            }
         scan.nextLine();
         System.out.println("Podaj PESEL:");
         String pesel = scan.nextLine();
